7e57043cde01c98c9d7a0aebf7cb57baa8d1bf62
asliddinotabekov <asyok005@gmail.com>
2024-03-03 13:55:56 +0500

    props && components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import { Student } from './Apple';
+import { Student, Department } from './App';
+
+const students = [
+  { id: 1, name: "Eshmat", age: 18 },
+  { id: 2, name: "Toshmat", age: 18 },
+  { id: 3, name: "Gulmat", age: 18 },
+  { id: 4, name: "Eshmamat", age: 18 },
+  { id: 5, name: "Asliddin", age: 18 },
+  { id: 6, name: "Ibrohim", age: 18 },
+]
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <div>
+
+    {
+      students.map((value) => {
+        return < Student data={value} />
+
+      })
+    }
+  </div>
 )
 
 
